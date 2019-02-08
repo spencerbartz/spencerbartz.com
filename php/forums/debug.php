005441e9f4cc84504f51868ac951c39bdaf5ba02
spencerbartz <spencerbartz@gmail.com>
2019-02-08 00:12:52 -0800

    start fixing db connection code

--- php/forums/debug.php
+++ php/forums/debug.php
@@ -6,12 +6,12 @@ Forum Debug
 </head>
 
 <?php
-include 'dbconnect.php';
+include '../../util/dbconnect.php';
 
 function printTable($conn, $table_name)
 {
 	$qhandle = mysql_query("SELECT * FROM " . $table_name);
-	
+
 	if(!$qhandle)
 		Die(mysql_error($conn));
 
@@ -19,16 +19,16 @@ function printTable($conn, $table_name)
 	{
 		echo $table_name . "<br>\n";
 		echo "<table border=1>";
-		
+
 		//$row is an associative array
 		$row = mysql_fetch_array($qhandle, MYSQL_ASSOC);
-		
+
 		//get the array keys, which will be the names of the column headers
 		$keys = array_keys($row);
-		
+
 		foreach($keys as $key)
 			echo "<th>" . $key . "</th>";
-		
+
 		do
 		{
 			echo "<tr>\n";
